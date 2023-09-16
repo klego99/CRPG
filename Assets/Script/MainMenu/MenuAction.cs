4ccab87dba2bc5f9be111ea9600eeda820c5c85a
klego99 <81143217+klego99@users.noreply.github.com>
2023-09-17 00:30:22 +0900

    Script add

---
+++ Assets/Script/MainMenu/MenuAction.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuAction : MonoBehaviour
+{
+    [SerializeField] Image Menu;
+    [SerializeField] Image TrainingMenu;
+    [SerializeField] Image FightMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void MyInfo()
+    {
+        
+    }
+    public void Training()
+    {
+
+    }
+    public void Store()
+    {
+
+    }
+    public void Equipment()
+    {
+
+    }
+    public void Event()
+    {
+
+    }
+    public void Fight()
+    {
+
+    }
+}
